856e572c9814043844c7fc323c91abe33ce55ca8
양정운 <yp071704@gmail.com>
2026-01-10 19:56:47 +0900

    feat: react query dev tools 비활성화 제거

--- src/providers/QueryClientProvider.tsx
+++ src/providers/QueryClientProvider.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
 
 export default function ReactQueryProvider({ children }: { children: React.ReactNode }) {
@@ -17,10 +16,5 @@ export default function ReactQueryProvider({ children }: { children: React.React
       })
   );
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-    </QueryClientProvider>
-  );
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
